ee2d73826ccc2ffda0381ea5dee74141deeeb408
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-09-12 03:04:32 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Boundary Traversal of binary tree/boundary-traversal-of-binary-tree.cpp
@@ -0,0 +1,181 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+
+
+
+
+
+
+
+// } Driver Code Ends
+/* A binary tree Node
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+class Solution
+{
+public:
+    bool isLeaf(Node* pre)
+    {
+        return (pre->left==NULL and pre->right==NULL);
+    }
+
+    void getleft(Node* root,vector<int> &v){
+        Node* pre = root->left;
+        while(pre){
+            if(!isLeaf(pre)) v.push_back(pre->data);
+            if(pre->left) pre = pre->left;
+            else  pre= pre->right;
+        }
+    }
+    void getleaf(Node *root,vector<int>&v){
+        
+        if(root==NULL) return;
+         
+        if(root->left)   getleaf(root->left,v);
+        if(isLeaf(root)){ 
+            v.push_back(root->data);
+         }
+        if(root->right)  getleaf(root->right,v);
+    }
+    
+    void  getright(Node *root,vector<int>&v){
+        vector<int>temp;
+        Node* pre = root->right;
+        while(pre){
+            if(!isLeaf(pre)) temp.push_back(pre->data);
+            if(pre->right) pre= pre->right;
+            else pre=pre->left;
+        }
+        
+        for(int i=temp.size()-1;i>=0;i--){
+            v.push_back(temp[i]);
+        }
+    }
+    
+    
+    vector <int> boundary(Node *root)
+    {
+      vector<int>v;
+       if(isLeaf(root))
+            return {root->data};
+      v.push_back(root->data);
+      getleft(root,v);
+      getleaf(root,v);
+      getright(root,v);
+      return v;
+    }
+};
+
+//{ Driver Code Starts.
+
+/* Driver program to test size function*/
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s ,ch;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        Solution ob;
+        vector <int> res = ob.boundary(root);
+        for (int i : res) cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
